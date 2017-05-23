668b9476fb12a7334b92cff460f3c8c9a672f57b
zcs-seu <18351959626@163.com>
2017-05-23 17:59:24 +0800

    add Main

---
+++ src/com/nowcoder/Main.java
@@ -0,0 +1,10 @@
+package com.nowcoder;
+
+/**
+ * Created by seu on 2017/5/23.
+ */
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello,World!");
+    }
+}
